e2a1eeaace12a95e872ac1491eea059201cc9af4
casper992 <60609364+casper992@users.noreply.github.com>
2020-12-17 19:32:56 +0300

    Add files via upload

--- js/diagram.js
+++ js/diagram.js
@@ -2,38 +2,47 @@ var ctx = document.getElementById('diagram').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        
         labels: ['','','', '','', '','', '',''],
         display: false,
         datasets: [{
             label: '',
             data: [0,83,58,72,55,95,54,85,0],
-            backgroundColor: [
-                'white'
-            ],
-            borderColor: [
-                'white'
-            ],
+            backgroundColor: ['#f6f6f6'],
+            hoverBackgroundColor: ['green'],
+            borderColor: ['white'],
             scaleLineColor: 'white',
-            
             pointBackgroundColor: 'black',
-            pointRadius: 5,
+            pointRadius: [0, 4, 0, 4, 0, 4, 0, 4, 0,],
             borderWidth: 1,
              
         }]
     },
     options: {
+        
         scales: {
+            xAxes : [{
+                gridLines : {
+                    display : false
+                },
+            }],
+
             yAxes: [{
+                render: 'percentage',
                 ticks: {
-                   
                     min: 50,
                     max: 100,
                     stepSize: 10,
                     beginAtZero: true,
+                    callback: function (value, index, values) {
+                        return value + '%';
+                    }
+                },
                 
-                }
             }]
-        }
+        },
     }
 });
+
+
+
+
